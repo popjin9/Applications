e1152fe9f30340c2d654c6cf1c5ecfa0ebc5c613
popjin9 <33330439+popjin9@users.noreply.github.com>
2018-07-26 22:38:47 +1000

    Got ilp-internet-1 working

--- ilp-internet-1/index.php
+++ ilp-internet-1/index.php
@@ -1,41 +1,79 @@
+<?php
+session_start();
+
+if ($_SESSION['opened'] !== 1){
+
+$_SESSION['opened'] = 1; 
+
+echo ("
 <!DOCTYPE html>
 
 <html>
 <head>
-  <meta charset="utf-8">
-  <title>Mirror ILP Test Site</title>
-  <link rel="stylesheet" href="static/base.css">
-  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+  <meta charset='utf-8'>
+  <title>Totally a site about vietnam helicopters and ILP</title>
+  <link rel='stylesheet' href='static/base.css'>
+  <link rel='icon' href='/favicon.ico' type='image/x-icon'>
 </head>
 
 <body>
 
 <header>
-  <h1>Mirror ILP Test Site</h1>
+  <h1>Totally a site about vietnam helicopters and ILP</h1>
 </header>
 
-<!--
-<section>
-
-<div id="form_wrapper">
-  <form action="" method="GET" accept-charset="utf-8">
-  <div id="input_wrapper">
-    <span id="http_prefix">http://</span>
-    <input type="text" name="url" placeholder="type url here..." id="url_entry" autofocus><input id="go_button" type="submit" value="Go">
-  </div>
-  </form>
-</div>
-
-<div id="warning">
-Fair use: All content belongs to the original copyright holders, respectively.
-</div>
-
 </section>
-!-->
 
 <body>
+<h3>NSW DET Proxy</h3>
+<h4>The proxy whitelists sites based on keywords such as engineering, technology and education.</h3>
+<p>In the case of the DET NSW, the program likely uses keywords of things related to education such as science, telescope, spectrometer, nebulae, and woodwork</p
+<p>The use of this sort of filtering is that it provides a quick and easy way to analyse and categorise sites based on their content, and requires minimum human intervention. Below, is some filler text to bypass this filtering.</p>
+<br>
 
-<?php
+<a href='./index2.php'>Click HERE for MSC Bypass system</a><br>
+
+<h4>What is Censorship?</h4>
+
+<p>Censorship is when an authority (such as a government or religion) cuts out or suppresses communication.
+This has been done widely. All countries, religions and societies have their limits as to what can be said, or written or communication by art or nowadays by computer.
+Certain facts are changed or removed on purpose. This may be done because it is considered wrong, harmful, sensitive, or inconvenient to the government or other authority. This can be done for different reasons.
+A censor is a person whose job is to look at all types of media and remove material. There are many reasons to censor something, like protecting military secrets, stopping immoral or anti-religious works, or keeping political power. Censorship is almost always used as an insult, and there is much debate over what censorship is and when it is okay.
+When there is freedom of speech and freedom of the press, most information can published. However, even in developed countries with much freedom of the press, there are some things that cannot be published. For example, journalists are usually not allowed to publish many secrets about the military, like where troops will be sent on a mission. </p>
+
+<p>Astronomers can gain an understanding of objects in the universe by studying them with a telescope. 
+Observational astronomy is the process of collecting and analyzing data to learn about these objects. 
+In this learning project computer programs will be used to view and study images or measurements taken with a telescope to learn basic concepts in astronomy.</p>
+
+<h4>What is a Proxy?</h4>
+
+<p>In computer networks, A proxy server is a server ( A computer system or
+an application ) that acts as an intermediary for requests from clients
+seeking resources from other servers. A client connects to the proxy
+server, requesting some service, such as a file, connection, web page,
+or other resource available from a different server and the proxy server
+evaluates the request as a way to simplify and control its complexity.
+
+Proxies were invented to add structure and encapsulation to distributed
+systems. Today, most proxies are web proxies, facilitating access to
+content on the World Wide Web and providing anonymity.
+
+What Bob thinks is the server ( i.e the proxy ) asked for the current time, 
+But what Bob didn't know was, Alice asked for the current time but through the proxy server. 
+The proxy server returns the current time to Alice. So we can basically say, Server Bob has been tricked. 
+The proxy server acts as a man in the middle serving two people without revealing their identities to each other, 
+Each person sees only the proxy but not the other end.
+
+While a proxy might seem straightforward to the typical end user, there are many variations of the simple proxy website. W
+hile you might want only you chat with Facebook friends while at work and need to hop online anonymously through a proxy, 
+what's happening behind the scenes can be quite complex. While many proxies look similar on the user's end, 
+they can be very different to the developers and programmers.</p><br>
+</body>
+
+</html>");
+}
+
+
 /*
 miniProxy - A simple PHP web proxy. <https://github.com/joshdick/miniProxy>
 Written and maintained by Joshua Dick <http://joshdick.net>.
@@ -53,7 +91,7 @@ $whitelistPatterns = array(
   //getHostnamePattern("example.net")
 );
 //To enable CORS (cross-origin resource sharing) for proxied sites, set $forceCORS to true.
-$forceCORS = false;
+$forceCORS = true;
 //Set to false to report the client machine's IP address to proxied sites via the HTTP `x-forwarded-for` header.
 //Setting to false may improve compatibility with some sites, but also exposes more information about end users to proxied sites.
 $anonymize = true;
@@ -281,7 +319,7 @@ if (isset($_POST["miniProxyFormAction"])) {
 }
 if (empty($url)) {
     if (empty($startURL)) {
-      die("<html><head><title>miniProxy</title></head><body><h1>Welcome to miniProxy!</h1>miniProxy can be directly invoked like this: <a href=\"" . PROXY_PREFIX . $landingExampleURL . "\">" . PROXY_PREFIX . $landingExampleURL . "</a><br /><br />Or, you can simply enter a URL below:<br /><br /><form onsubmit=\"if (document.getElementById('site').value) { window.location.href='" . PROXY_PREFIX . "' + document.getElementById('site').value; return false; } else { window.location.href='" . PROXY_PREFIX . $landingExampleURL . "'; return false; }\" autocomplete=\"off\"><input id=\"site\" type=\"text\" size=\"50\" /><input type=\"submit\" value=\"Proxy It!\" /></form></body></html>");
+      die("<h2>Vietnam Helicopters aka the MSC Proxy Bypass System</h2>URLs can be input like this: <a href=\"" . PROXY_PREFIX . $landingExampleURL . "\">" . PROXY_PREFIX . $landingExampleURL . "</a><br /><br />Or, you can simply enter a URL below:<br /><br /><form onsubmit=\"if (document.getElementById('site').value) { window.location.href='" . PROXY_PREFIX . "' + document.getElementById('site').value; return false; } else { window.location.href='" . PROXY_PREFIX . $landingExampleURL . "'; return false; }\" autocomplete=\"off\"><input id=\"site\" type=\"text\" size=\"50\" /><input type=\"submit\" value=\"Go\" /></form><br><br><br><br><br><br><br><br><br><br><br><br>");
     } else {
       $url = $startURL;
     }
@@ -298,7 +336,7 @@ if (empty($scheme)) {
     $url = "http:" . $url;
   }
 } else if (!preg_match("/^https?$/i", $scheme)) {
-    die('Error: Detected a "' . $scheme . '" URL. miniProxy exclusively supports http[s] URLs.');
+    die('Error: Detected a "' . $scheme . '" URL. Please use http[s] URLs.');
 }
 //Validate the requested URL against the whitelist.
 $urlIsValid = count($whitelistPatterns) === 0;
@@ -497,7 +535,7 @@ if (stripos($contentType, "text/html") !== false) {
     $scriptElem->setAttribute("type", "text/javascript");
     $prependElem->insertBefore($scriptElem, $prependElem->firstChild);
   }
-  echo "<!-- Proxified page constructed by miniProxy -->\n" . $doc->saveHTML();
+  echo "<!-- -->\n" . $doc->saveHTML();
 } else if (stripos($contentType, "text/css") !== false) { //This is CSS, so proxify url() references.
   echo proxifyCSS($responseBody, $url);
 } else { //This isn't a web page or CSS, so serve unmodified through the proxy with the correct headers (images, JavaScript, etc.)
@@ -505,59 +543,3 @@ if (stripos($contentType, "text/html") !== false) {
   echo $responseBody;
 }
 ?>
-
-
-<br>
-<h2>NSW DET Proxy</h2>
-<h3>The proxy whitelists sites based on keywords such as engineering, technology and education - below for further details</h3
-<p>Censorship is when an authority (such as a government or religion) cuts out or suppresses communication.
-This has been done widely. All countries, religions and societies have their limits as to what can be said, or written or communication by art or nowadays by computer.
-Certain facts are changed or removed on purpose. This may be done because it is considered wrong, harmful, sensitive, or inconvenient to the government or other authority. This can be done for different reasons.
-A censor is a person whose job is to look at all types of media and remove material. There are many reasons to censor something, like protecting military secrets, stopping immoral or anti-religious works, or keeping political power. Censorship is almost always used as an insult, and there is much debate over what censorship is and when it is okay.
-When there is freedom of speech and freedom of the press, most information can published. However, even in developed countries with much freedom of the press, there are some things that cannot be published. For example, journalists are usually not allowed to publish many secrets about the military, like where troops will be sent on a mission. </p><br>
-<p>Astronomers can gain an understanding of objects in the universe by studying them with a telescope. 
-Observational astronomy is the process of collecting and analyzing data to learn about these objects. 
-In this learning project computer programs will be used to view and study images or measurements taken with a telescope to learn basic concepts in astronomy.</p><br>
-<p>INTRODUCTION INTO THE WORLD OF PROXIES
-
-From WikiPedia - Proxy Server.
-
-In computer networks, A proxy server is a server ( A computer system or
-an application ) that acts as an intermediary for requests from clients
-seeking resources from other servers. A client connects to the proxy
-server, requesting some service, such as a file, connection, web page,
-or other resource available from a different server and the proxy server
-evaluates the request as a way to simplify and control its complexity.
-
-Proxies were invented to add structure and encapsulation to distributed
-systems. Today, most proxies are web proxies, facilitating access to
-content on the World Wide Web and providing anonymity.
-
-What Bob thinks is the server ( i.e the proxy ) asked for the current time, 
-But what Bob didn't know was, Alice asked for the current time but through the proxy server. 
-The proxy server returns the current time to Alice. So we can basically say, Server Bob has been tricked. 
-The proxy server acts as a man in the middle serving two people without revealing their identities to each other, 
-Each person sees only the proxy but not the other end.
-
-While a proxy might seem straightforward to the typical end user, there are many variations of the simple proxy website. W
-hile you might want only you chat with Facebook friends while at work and need to hop online anonymously through a proxy, 
-what's happening behind the scenes can be quite complex. While many proxies look similar on the user's end, 
-they can be very different to the developers and programmers.</p><br>
-
-<h1>DET NSW</h1>
-<p>In the case of the DET NSW, the program likely uses keywords of things related to education such as science, telescope, spectrometer, nebulae, and woodwork</p
-<p>The use of this sort of filtering is that it provides a quick and easy way to analyse and categorise sites based on their content, and requires minimum human intervention</p>
-
-<!--
-<footer class="secure">
-  {% if secure_url %}
-  <a href="{{secure_url}}">
-  {% endif %}
-  <img src="/static/{% if secure_url %}no{% endif %}lock.png" width="16" height="16">
-  {% if secure_url %}not secure</a>
-  {% else %}secure{% endif %}
-</footer>
-!-->
-
-</body>
-</html>
---
+++ ilp-internet-1/indexold.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Totally a site about vietnam helicopters and ILP</title>
+  <link rel="stylesheet" href="static/base.css">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+
+<header>
+  <h1>Totally a site about vietnam helicopters and ILP</h1>
+</header>
+
+<!--
+<section>
+
+<div id="form_wrapper">
+  <form action="" method="GET" accept-charset="utf-8">
+  <div id="input_wrapper">
+    <span id="http_prefix">http://</span>
+    <input type="text" name="url" placeholder="type url here..." id="url_entry" autofocus><input id="go_button" type="submit" value="Go">
+  </div>
+  </form>
+</div>
+
+<div id="warning">
+Fair use: All content belongs to the original copyright holders, respectively.
+</div>
+
+</section>
+!-->
+
+<body>
+<h3>NSW DET Proxy</h3>
+<h4>The proxy whitelists sites based on keywords such as engineering, technology and education.</h3>
+<p>In the case of the DET NSW, the program likely uses keywords of things related to education such as science, telescope, spectrometer, nebulae, and woodwork</p
+<p>The use of this sort of filtering is that it provides a quick and easy way to analyse and categorise sites based on their content, and requires minimum human intervention. Below, is some filler text to bypass this filtering.</p>
+<br>
+
+<a href="./index2.php">Click HERE for MSC Bypass system</a><br>
+
+<h4>What is Censorship?</h4>
+
+<p>Censorship is when an authority (such as a government or religion) cuts out or suppresses communication.
+This has been done widely. All countries, religions and societies have their limits as to what can be said, or written or communication by art or nowadays by computer.
+Certain facts are changed or removed on purpose. This may be done because it is considered wrong, harmful, sensitive, or inconvenient to the government or other authority. This can be done for different reasons.
+A censor is a person whose job is to look at all types of media and remove material. There are many reasons to censor something, like protecting military secrets, stopping immoral or anti-religious works, or keeping political power. Censorship is almost always used as an insult, and there is much debate over what censorship is and when it is okay.
+When there is freedom of speech and freedom of the press, most information can published. However, even in developed countries with much freedom of the press, there are some things that cannot be published. For example, journalists are usually not allowed to publish many secrets about the military, like where troops will be sent on a mission. </p>
+
+<p>Astronomers can gain an understanding of objects in the universe by studying them with a telescope. 
+Observational astronomy is the process of collecting and analyzing data to learn about these objects. 
+In this learning project computer programs will be used to view and study images or measurements taken with a telescope to learn basic concepts in astronomy.</p>
+
+<h4>What is a Proxy?</h4>
+
+<p>In computer networks, A proxy server is a server ( A computer system or
+an application ) that acts as an intermediary for requests from clients
+seeking resources from other servers. A client connects to the proxy
+server, requesting some service, such as a file, connection, web page,
+or other resource available from a different server and the proxy server
+evaluates the request as a way to simplify and control its complexity.
+
+Proxies were invented to add structure and encapsulation to distributed
+systems. Today, most proxies are web proxies, facilitating access to
+content on the World Wide Web and providing anonymity.
+
+What Bob thinks is the server ( i.e the proxy ) asked for the current time, 
+But what Bob didn't know was, Alice asked for the current time but through the proxy server. 
+The proxy server returns the current time to Alice. So we can basically say, Server Bob has been tricked. 
+The proxy server acts as a man in the middle serving two people without revealing their identities to each other, 
+Each person sees only the proxy but not the other end.
+
+While a proxy might seem straightforward to the typical end user, there are many variations of the simple proxy website. W
+hile you might want only you chat with Facebook friends while at work and need to hop online anonymously through a proxy, 
+what's happening behind the scenes can be quite complex. While many proxies look similar on the user's end, 
+they can be very different to the developers and programmers.</p><br>
+</body>
+
+<body>
+
+<!--
+<footer class="secure">
+  {% if secure_url %}
+  <a href="{{secure_url}}">
+  {% endif %}
+  <img src="/static/{% if secure_url %}no{% endif %}lock.png" width="16" height="16">
+  {% if secure_url %}not secure</a>
+  {% else %}secure{% endif %}
+</footer>
+!-->
+
+</body>
+</html>
